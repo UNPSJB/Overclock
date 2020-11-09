ce361aec3e6e4f6aa0a323559055e85bbdb679b2
damianmartinez1982 <correo2012.damian@gmail.com>
2020-11-09 18:39:25 -0300

    arreglo de librerias e iconos, a locales
    arreglo de librerias e iconos, a locales

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+/*script para abrir modales recibe por parametros la url y el id*/
+
+    function abrir_modal(url, id) { /// al llamar a la funcion para apertura de modales le damos url del modal y id del mismo
+        $(id).load(url, function () {
+            $(this).modal('show');
+        });
+    }
