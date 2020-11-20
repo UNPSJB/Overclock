9ca7eacdf75757799bf883ad0166731cec885431
Diego van Haaster <diegomvh@gmail.com>
2020-11-20 18:12:59 -0300

    Comento modal show

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,6 @@
 
     function abrir_modal(url, id) { /// al llamar a la funcion para apertura de modales le damos url del modal y id del mismo
         $(id).load(url, function () {
-            $(this).modal('show');
+            //$(this).modal('show');
         });
     }
